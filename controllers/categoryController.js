945d4c765f7a22801f8e79d450d377ba496d8cd5
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-08-13 19:19:52 +0100

    added controllers

---
+++ controllers/categoryController.js
@@ -0,0 +1,41 @@
+var Category = require('../models/category');
+
+// Display list of all categories
+exports.category_list = function (req, res) {
+    res.send('Not Implemented: Category List')
+}
+
+// Display Details of categories
+exports.category_detail = function (req, res) {
+    res.send('Not Implemented: Category Deatis')
+}
+
+// Display Category Create form on GET
+exports.category_create_get = function (req, res) {
+    res.send('Not Implemented: Category Create GET')
+}
+
+// Handle Category Create on POST
+exports.category_create_post = function (req, res) {
+    res.send('Not Implemented: Category Create POST')
+}
+
+// Display Category Delete form on GET
+exports.category_delete_get = function (req, res) {
+    res.send('Not Implemented: Category Delete GET')
+}
+
+// Handle Category Delete on POST
+exports.category_delete_post = function (req, res) {
+    res.send('Not Implemented: Category Delete POST')
+}
+
+// Display Category Update form on GET
+exports.category_update_get = function (req, res) {
+    res.send('Not Implemented: Category Update Get')
+}
+
+// Handle Category Update on POST
+exports.category_update_post = function (req, res) {
+    res.send('Not Implemented: Category Update POST')
+}
---
+++ controllers/itemController.js
@@ -0,0 +1,41 @@
+var Item = require('../models/item');
+
+// Display list of all Items
+exports.item_list = function (req, res) {
+    res.send('Not Implemented: Item List')
+}
+
+// Display Details of Items
+exports.item_detail = function (req, res) {
+    res.send('Not Implemented: Item Deatis')
+}
+
+// Display Item Create form on GET
+exports.item_create_get = function (req, res) {
+    res.send('Not Implemented: Item Create GET')
+}
+
+// Handle Item Create on POST
+exports.item_create_post = function (req, res) {
+    res.send('Not Implemented: Item Create POST')
+}
+
+// Display Item Delete form on GET
+exports.item_delete_get = function (req, res) {
+    res.send('Not Implemented: Item Delete GET')
+}
+
+// Handle Item Delete on POST
+exports.item_delete_post = function (req, res) {
+    res.send('Not Implemented: Item Delete POST')
+}
+
+// Display Item Update form on GET
+exports.item_update_get = function (req, res) {
+    res.send('Not Implemented: Item Update Get')
+}
+
+// Handle Item Update on POST
+exports.item_update_post = function (req, res) {
+    res.send('Not Implemented: Item Update POST')
+}
